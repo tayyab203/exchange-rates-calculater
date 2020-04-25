866f21a0e09ffb326ab9b8b9bbdde5c11f8b0996
tayyab203 <61288485+tayyab203@users.noreply.github.com>
2020-04-25 14:13:52 -0700

    commit 7

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const currencyEl_one = document.getElementById('currency-one');
-const amountEl_one = document.getElementById('amount-one');
-const currencyEl_two = document.getElementById('currency-two');
-const amountEl_two = document.getElementById('amount-two');
+const currencyEl_one = document.getElementById("currency-one");
+const amountEl_one = document.getElementById("amount-one");
+const currencyEl_two = document.getElementById("currency-two");
+const amountEl_two = document.getElementById("amount-two");
 
-const rateEl = document.getElementById('rate');
-const swap = document.getElementById('swap');
+const rateEl = document.getElementById("rate");
+const swap = document.getElementById("swap");
 
 // Fetch exchange rates and update the DOM
 function calculate() {
@@ -12,8 +12,8 @@ function calculate() {
   const currency_two = currencyEl_two.value;
 
   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
       // console.log(data);
       const rate = data.rates[currency_two];
 
@@ -24,17 +24,16 @@ function calculate() {
 }
 
 // Event listeners
-currencyEl_one.addEventListener('change', calculate);
-amountEl_one.addEventListener('input', calculate);
-currencyEl_two.addEventListener('change', calculate);
-amountEl_two.addEventListener('input', calculate);
+currencyEl_one.addEventListener("change", calculate);
+amountEl_one.addEventListener("input", calculate);
+currencyEl_two.addEventListener("change", calculate);
+amountEl_two.addEventListener("input", calculate);
 
-
-swap.addEventListener('click', () => {
-    const temp = currencyEl_one.value;
-    currencyEl_one.value = currencyEl_two.value;
-    currencyEl_two.value = temp;
-    calculate();
-  });
-  
+swap.addEventListener("click", () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
   calculate();
+});
+
+calculate();
